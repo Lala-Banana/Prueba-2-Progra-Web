fe18c57c5379d0e9e303946255b8f31c039dfaa6
jordanbarraduran <jor.barra@duocuc.cl>
2024-05-22 22:19:16 -0400

    cambios en js

--- js/jquery.js
+++ js/jquery.js
@@ -1,7 +1,38 @@
-$(document).ready(function(){
-  $('a.nav-link').on('click', function(e){
-      e.preventDefault();
-      var target = $(this).data('target');
-      $('#contenido').load(target);
+$(document).ready(function() {
+  // Función para cargar contenido
+  function cargarContenido(archivo) {
+      $("#contenido").load(archivo);
+      console.log("Cargando " + archivo);
+  }
+
+  // Previene el comportamiento por defecto de los enlaces
+  $(".nav-link").off("click").on("click", function(event) {
+      event.preventDefault();
+      const id = $(this).attr('id');
+      switch (id) {
+          case "link-maceteros":
+              cargarContenido('maceteros.html');
+              break;
+          case "link-sustratos":
+              cargarContenido('sustratos.html');
+              break;
+          case "link-flores":
+              cargarContenido('flores.html');
+              break;
+          case "link-arbustos":
+              cargarContenido('arbustos.html');
+              break;
+          case "link-carrito":
+              cargarContenido('carrito.html');
+              break;
+              /*
+          case "link-iniciar-sesion":
+              cargarContenido('iniciosesion.html');
+              break;
+          case "link-registrarse":
+              cargarContenido('registro.html');
+              break;
+              */
+      }
   });
 });
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $("#registroForm").submit(function(event){
+$(document).ready(function() {
+    $("#registroForm").submit(function(event) {
         // Evitar que el formulario se envíe automáticamente
         event.preventDefault();
         
@@ -14,50 +14,81 @@ $(document).ready(function(){
 
         var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9_\.\-]+$/;
 
+        // Validación de bandera
+        var valido = true;
+
         // Rut: largo entre 9 y 10 caracteres
-        if(rut.length < 9 || rut.length > 10){
+        if(rut.length < 9 || rut.length > 10) {
             alert("El Rut debe tener entre 9 y 10 caracteres.");
-            return;
+            valido = false;
         }
 
         // Nombre, Apellidos: largo entre 3 y 20 caracteres
-        if(nombre.length < 3 || nombre.length > 20 ||
-            apellido.length < 3 || apellido.length > 20){
-            alert("El Nombre y  Apellido deben tener entre 3 y 20 caracteres.");
-            return;
+        if(nombre.length < 3 || nombre.length > 20 || apellido.length < 3 || apellido.length > 20) {
+            alert("El Nombre y Apellido deben tener entre 3 y 20 caracteres.");
+            valido = false;
         }
 
         // Formato correo
-        if (!expr.test(correo)){
+        if (!expr.test(correo)) {
             alert("Formato de correo invalido.");
-            return;
+            valido = false;
         }
 
         // Edad: Validación de edad 
-        if (edad <18){
+        if (edad < 18) {
             alert("Debes ser mayor de edad.");
-            return;
+            valido = false;
         }
 
         // Género: seleccionado
-        if(genero === ""){
+        if(genero === "") {
             alert("Por favor, seleccione un Género.");
-            return;
+            valido = false;
         }
 
         // Celular: largo entre 9 y 12 caracteres y numérico
         var celularRegex = /^[0-9]+$/;
-        if(celular.length < 9 || celular.length > 12 || !celularRegex.test(celular)){
+        if(celular.length < 9 || celular.length > 12 || !celularRegex.test(celular)) {
             alert("El Celular debe tener entre 9 y 12 caracteres numéricos.");
-            return;
+            valido = false;
         }
 
         // Si todas las validaciones pasan, se puede enviar el formulario
-        alert("¡Registro exitoso!");
-        // Aquí podrías enviar el formulario utilizando AJAX o cualquier otro método
-        
-        // Limpiar los campos del formulario
-        $("#registroForm")[0].reset();
-   
+        if(valido) {
+            alert("¡Registro exitoso! Se le enviará un código de inicio de sesión al correo electrónico.");
+            // Aquí podrías enviar el formulario utilizando AJAX o cualquier otro método
+            
+            // Limpiar los campos del formulario
+            $("#registroForm")[0].reset();
+        }
     });
+
+    $("#inicioForm").submit(function(event) {
+        // Evitar que el formulario se envíe automáticamente
+        event.preventDefault();
+    
+        // Realizar las validaciones
+        var correo = $("#correo").val();
+        
+        var expr = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9_\.\-]+$/;
+
+        var valido = true;
+        // Formato correo
+        if (!expr.test(correo)) {
+            alert("Formato de correo invalido.");
+            valido = false;
+        }
+        
+        if (valido) {
+            
+            alert("Funcionalidad inhabilitada temporalmente.");
+            
+            // Limpiar los campos del formulario
+            $("#inicioForm")[0].reset();
+            
+        }
+    });
+    
 });
+
